9c21f913f91ef70f1509b59f7a33ae202427a29e
Randy D. Wallace Jr <randy@edatasource.com>
2017-07-31 03:43:41 -0400

    v0.0.10 Release

---
+++ push_release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "${1}x" == "x" ]; then echo "Must pass new version"; exit 1; fi
+
+sed -ri '0,/(<version>)[0-9]+\.[0-9]+\.[0-9]+(<\/version>)/s//\1'"$1"'\2/' pom.xml
+
+git add pom.xml
+
+git commit -m 'v'"${1}"' Release'
+
+git tag -a -m 'v'"${1}"' Release' "${1}"
+
+git push --tags origin dev
